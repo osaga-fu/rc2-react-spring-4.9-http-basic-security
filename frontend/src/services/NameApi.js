adaad33ec36bdb6cfaa5dd3ab1b8d48c1845d396
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-05 11:47:29 +0100

    implementada seguridad básica

--- frontend/src/services/NameApi.js
+++ frontend/src/services/NameApi.js
@@ -1,37 +1,49 @@
 const URL = "http://localhost:8080/members";
+const Authorization = "user:password";
+const hash = btoa(Authorization);
+
 export class NameApi {
-    getMembers() {
-        return fetch(URL)
-            .then(response => response.json())
+  getMembers() {
+    const headers = {
+      method: "get",
+      headers: {
+        Authorization: `Basic ${hash}`,
+      },
+    };
+    return fetch(URL, headers).then((response) => response.json());
+  }
 
-    }
+  addMember(member) {
+    const headers = {
+      method: "post",
+      headers: {
+        Authorization: `Basic ${hash}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(member),
+    };
+    return fetch(URL, headers).then((response) => response.json());
+  }
 
-    addMember(member) {
-        const headers = {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(member)
-        };
-        return fetch(URL, headers)
-            .then(response => response.json())
+  deleteMember(id) {
+    const headers = {
+      method: "delete",
+      headers: {
+        Authorization: `Basic ${hash}`,
+      },
+    };
+    return fetch(URL + "/" + id, headers).then((response) => response.json());
+  }
 
-    }
-
-    deleteMember(id) {
-        const headers = {
-            method: 'delete'
-        }
-        return fetch(URL + "/" + id, headers)
-            .then(response => response.json())
-    }
-
-    editMember(id, member) {
-        const headers = {
-            method: 'put',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(member)
-        }
-        return fetch(URL + "/" + id, headers)
-            .then(response => response.json())
-    }
+  editMember(id, member) {
+    const headers = {
+      method: "put",
+      headers: {
+        Authorization: `Basic ${hash}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(member),
+    };
+    return fetch(URL + "/" + id, headers).then((response) => response.json());
+  }
 }
